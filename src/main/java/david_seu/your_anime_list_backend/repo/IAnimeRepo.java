944c0b059888372d9ce23335a0c9c887b5a4bfd6
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-29 11:26:08 +0300

    done

--- src/main/java/david_seu/your_anime_list_backend/repo/IAnimeRepo.java
+++ src/main/java/david_seu/your_anime_list_backend/repo/IAnimeRepo.java
@@ -1,6 +1,7 @@
 package david_seu.your_anime_list_backend.repo;
 
 import david_seu.your_anime_list_backend.model.Anime;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import java.util.List;
 
 @Repository
 public interface IAnimeRepo extends JpaRepository<Anime, Long> {
+
+    List<Anime> findAnimeByOrderByScore(PageRequest pageRequest);
+
 }
